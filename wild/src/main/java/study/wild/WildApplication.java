bc4cb9912c00fc61e46b78d8859ed15cdad0669c
soddong <aledjdygus9300@gmail.com>
2023-12-13 22:15:10 +0900

    feat: Add 'BaseTimeEntity' class for time managing

--- wild/src/main/java/study/wild/WildApplication.java
+++ wild/src/main/java/study/wild/WildApplication.java
@@ -2,12 +2,14 @@ package study.wild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WildApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WildApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WildApplication.class, args);
+    }
 
 }
