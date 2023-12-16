beac45b2f3d154a1c7296c866ace319ee6c6a4fd
soddong <aledjdygus9300@gmail.com>
2023-12-16 20:39:33 +0900

    style: Clean up code

--- wild/src/main/java/study/wild/service/PostService.java
+++ wild/src/main/java/study/wild/service/PostService.java
@@ -3,8 +3,6 @@ package study.wild.service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Filter;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- wild/src/test/java/study/wild/service/CommentServiceTest.java
+++ wild/src/test/java/study/wild/service/CommentServiceTest.java
@@ -1,21 +1,17 @@
 package study.wild.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-import study.wild.domain.Comment;
 import study.wild.dto.CommentDto;
 import study.wild.dto.PostDto;
-import study.wild.repository.CommentRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
