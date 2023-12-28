366023bc5d101992b5527e472d483f7606af3ed4
soddong <aledjdygus9300@gmail.com>
2023-12-28 15:31:49 +0900

    remove: Remove unused codes

--- wild/src/main/java/study/wild/common/exception/EmptyInputException.java
+++
@@ -1,9 +0,0 @@
-package study.wild.common.exception;
-
-public class EmptyInputException extends RuntimeException {
-    private static String MESSAGE = "Value is empty";
-
-    public EmptyInputException() {
-        super(MESSAGE);
-    }
-}
