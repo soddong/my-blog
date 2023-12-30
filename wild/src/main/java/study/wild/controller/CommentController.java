17ad06c8fa431c3f34a1cc66553b596ebc2bcb33
soddong <aledjdygus9300@gmail.com>
2023-12-31 01:31:21 +0900

    refactor: Centralize CORS configuration in WebConfig

--- wild/src/main/java/study/wild/controller/CommentController.java
+++ wild/src/main/java/study/wild/controller/CommentController.java
@@ -2,14 +2,20 @@ package study.wild.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import study.wild.dto.CommentDto;
 import study.wild.service.CommentService;
 import study.wild.service.PostCommentService;
 
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
