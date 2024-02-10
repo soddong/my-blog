38754e3487cd4794ca4df812e44eb4a4baacfbdd
soddong <aledjdygus9300@gmail.com>
2024-02-11 01:11:21 +0900

    feat: Add post create

--- react-client/src/components/PostForm.js
+++ react-client/src/components/PostForm.js
@@ -1,46 +1,51 @@
 import React, { useState } from 'react';
+import '../css/post.css'; // post.css 파일 import
 
 const PostForm = ({ onAddPost, categories }) => {
-    const [post, setPost] = useState({ id: null, categoryId: '', title: '', content: '' });
+  const [post, setPost] = useState({ id: null, categoryId: '', title: '', content: '' });
 
-    const handleChange = e => {
-        setPost({ ...post, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e) => {
+    setPost({ ...post, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        onAddPost(post);
-        setPost({ id: null, title: '', content: '', categoryId: '' });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddPost(post);
+    setPost({ id: null, title: '', content: '', categoryId: '' });
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text"
-                name="title"
-                value={post.title}
-                onChange={handleChange}
-                placeholder="제목"
-            />
-            <textarea 
-                name="content"
-                value={post.content}
-                onChange={handleChange}
-                placeholder="내용"
-            />
-            <select 
-                name="categoryId"
-                value={post.categoryId}
-                onChange={handleChange}
-            >
-                <option value="">카테고리 선택</option>
-                {categories.map(category => (
-                    <option key={category.id} value={category.id}>{category.name}</option>
-                ))}
-            </select>
-            <button type="submit">게시글 등록</button>
-        </form>
-    );
+  return (
+    <div className="post-form"> {/* post-form 클래스 추가 */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={post.title}
+          onChange={handleChange}
+          placeholder="제목"
+        />
+        <textarea
+          name="content"
+          value={post.content}
+          onChange={handleChange}
+          placeholder="내용"
+        />
+        <select
+          name="categoryId"
+          value={post.categoryId}
+          onChange={handleChange}
+        >
+          <option value="">카테고리 선택</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit">게시글 등록</button>
+      </form>
+    </div>
+  );
 };
 
 export default PostForm;
--- react-client/src/pages/posts/Posts.js
+++ react-client/src/pages/posts/Posts.js
@@ -1,22 +1,25 @@
+// Posts.js
 import React, { useEffect, useState } from 'react';
 import { categoryService } from '../../service/categoryService';
 import { postService } from '../../service/postService';
-import PostList from '../../components/PostList'; // Import PostList component
+import PostForm from '../../components/PostForm';
 import '../../css/post.css';
+import { useNavigate } from 'react-router-dom';
 
 const Posts = () => {
+  const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
   const [posts, setPosts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedPostId, setSelectedPostId] = useState(null);
+  const [isExpandedPost, setIsExpandedPost] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
-  
+
   useEffect(() => {
     loadCategories();
     loadInitialPosts();
   }, []);
 
-  // Load initial posts without category filter
   const loadInitialPosts = async () => {
     try {
       const response = await postService.getPosts();
@@ -35,29 +38,38 @@ const Posts = () => {
     }
   };
 
-  const onSelectCategory = async categoryId => {
+  const onSelectCategory = async (categoryId) => {
     try {
       setSelectedCategory(categoryId);
       const response = await postService.getPosts(categoryId);
       setPosts(response.data);
+      closeExpandedView();
     } catch (error) {
       console.error(`Error fetching posts for category ${categoryId}:`, error);
     }
   };
 
-  const onSelectPost = postId => {
+  const onSelectPost = (postId) => {
     setSelectedPostId(postId);
-    setIsExpanded(true); // Set the post to be expanded
+    setIsExpandedPost(true);
   };
 
   const closeExpandedView = () => {
-    setSelectedPostId(null); // Reset the selected post
-    setIsExpanded(false); // Reset the expanded view
+    setSelectedPostId(null);
+    setIsExpandedPost(false);
+    navigate('/posts');
   };
-  
-  
 
-  const selectedPost = posts.find(post => post.id === selectedPostId);
+  const addPost = async (newPost) => {
+    try {
+      const response = await postService.addPost(newPost);
+      loadInitialPosts();
+    } catch (error) {
+      console.error('Error adding post:', error);
+    }
+  };
+
+  const selectedPost = posts.find((post) => post.id === selectedPostId);
 
   return (
     <div>
@@ -65,13 +77,12 @@ const Posts = () => {
         <div className="category-box">
           <h2>카테고리</h2>
           <ul id="category-list">
-            {/* 전체보기 옵션 추가 */}
             <li key="all" onClick={() => onSelectCategory(null)}>
               전체보기
             </li>
             {categories
-              .filter(category => category.name !== "default")
-              .map(category => (
+              .filter((category) => category.name !== 'default')
+              .map((category) => (
                 <li key={category.id} onClick={() => onSelectCategory(category.id)}>
                   {category.name}
                 </li>
@@ -80,30 +91,39 @@ const Posts = () => {
         </div>
       </aside>
 
-
       <main>
-      <div className="posts-container">
-        {posts.map(post => (
-          <div key={post.id} className={`post-item ${selectedPostId === post.id && isExpanded ? 'expanded' : ''}`} onClick={() => onSelectPost(post.id)}>
-            <h3>{post.title}</h3>
-            {selectedPostId === post.id && isExpanded && (
-              <div className="expanded-content">
-                <button className="close-btn" onClick={closeExpandedView}>Close</button>
-                {selectedPost ? (
-                  <>
-                    <p>{selectedPost.content}</p>
-                    {/* Add more expanded post details here */}
-                  </>
-                ) : (
-                  // selectedPost가 null이면 리스트를 출력
-                  <p>{post.content}</p>
-                )}
-              </div>
-            )}
+        <div className="posts-container">
+          <div className='add-post-button'>
+            <button onClick={() => setIsExpanded(true)}>게시글 등록하기</button>
           </div>
-        ))}
-</div>
 
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              className={`post-item ${selectedPostId === post.id && isExpandedPost ? 'expanded' : ''}`}
+              onClick={() => onSelectPost(post.id)}
+            >
+              <h3>{post.title}</h3>
+              {selectedPostId === post.id && isExpandedPost && (
+                <div className="expanded-content">
+                  <button className="close-btn" onClick={closeExpandedView}>
+                    Close
+                  </button>
+                  {selectedPost ? (
+                    <>
+                      <p>{selectedPost.content}</p>
+                      {/* Add more expanded post details here */}
+                    </>
+                  ) : (
+                    <p>{post.content}</p>
+                  )}
+                </div>
+              )}
+            </div>
+          ))}
+          {isExpanded && <PostForm onAddPost={addPost} categories={categories} className='post-form' />}
+
+        </div>
       </main>
     </div>
   );
