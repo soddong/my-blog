5903f619eda546d4eae0babac7d974023dd77e55
soddong <aledjdygus9300@gmail.com>
2024-02-25 16:41:32 +0900

    feat: Add periodic session check in client code

--- react-client/src/pages/login/LoginContext.js
+++ react-client/src/pages/login/LoginContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
+import { loginService } from '../../service/loginService';
 
 const LoginContext = createContext();
 
@@ -8,6 +9,24 @@ export const useLoginContext = () => {
 
 export const LoginProvider = ({ children }) => {
   const [loginSession, setLoginSession] = useState(false);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const response = await loginService.fetchSession();
+        if (response.status === 200) {
+          setLoginSession(true);
+        } else {
+          setLoginSession(false);
+        }
+      } catch (error) {
+        console.error('세션 확인 실패:', error);
+        setLoginSession(false);
+      }
+    };
+
+    checkSession();
+  }, []);
 
   const login = () => {
     setLoginSession(true);
--- react-client/src/service/loginService.js
+++ react-client/src/service/loginService.js
@@ -12,6 +12,19 @@ const login = async (name, password) => {
     throw error; // 오류를 상위로 전파하거나, 다르게 처리
   }
 };
+
+const fetchSession = async () => {
+  try {
+    const response = await axios.get(`${API_URL}api/auth/session`,{ withCredentials: true });
+    console.log(response.data); // 성공적으로 응답을 받은 경우
+    return response;
+  } catch (error) {
+    console.error(error); // 요청 중 에러가 발생한 경우
+    throw error;
+  }
+};
+
 export const loginService = {
-  login
+  login,
+  fetchSession
 };
