b654bb7c910b6320460fae4bb9d940df63b7ec34
soddong <aledjdygus9300@gmail.com>
2024-03-02 23:41:48 +0900

    config (CORS): Update allowed origins to release ip address

--- server/src/main/java/study/wild/config/WebConfig.java
+++ server/src/main/java/study/wild/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://3.38.66.238")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowCredentials(true)
